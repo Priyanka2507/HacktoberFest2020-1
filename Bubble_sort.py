d5159e1fac3cdaf2786075763d9c6c6816c07d25
Priyanka2507 <55374532+Priyanka2507@users.noreply.github.com>
2020-10-10 13:46:25 +0530

    Update Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -2,10 +2,12 @@ def bubbleSort(nlist):
     for passnum in range(len(nlist)-1,0,-1):
         for i in range(passnum):
             if nlist[i]>nlist[i+1]:
-                temp = nlist[i]
-                nlist[i] = nlist[i+1]
-                nlist[i+1] = temp
+                 nlist[i+1] , nlist[i] = nlist[i] ,  nlist[i+1]
 
-nlist = [14,46,43,27,57,41,45,21,70]
+                    
+n = int(input("Enter no. of elements")
+nlist = []
+for i in range(n):
+    nlist.append(int(input())
 bubbleSort(nlist)
 print(nlist)
